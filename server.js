f0ad7c1833e8bc8880631f74fd01b0f7b08de4bb
Simm <66415443+simm123@users.noreply.github.com>
2022-12-08 16:25:14 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
 const http = require('http');
+const server = http.createServer((req, res) =>{
+    console.log(req.url)
+    console.log(req.method);
+    console.log(req.headers);
+    res.setHeader
+});
+server.listen(187);
